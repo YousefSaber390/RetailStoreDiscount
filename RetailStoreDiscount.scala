df7198b1631231e09a320da1df6a1fe7599fc4fa
YousefSaber390 <147419924+YousefSaber390@users.noreply.github.com>
2024-05-08 00:44:12 +0300

    Add files via upload

---
+++ RetailStoreDiscount.scala
@@ -0,0 +1,118 @@
+import java.io.{File, PrintWriter}
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDate, Period}
+
+object RetailStoreDiscount {
+
+  // Define a case class to represent a transaction
+  case class Transaction(timestamp: String, productName: String, expiryDate: String, quantity: Int, unitPrice: Double, channel: String, paymentMethod: String) {
+    // Define date formatters for parsing dates
+    private val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    // Convert expiryDate and timestamp to LocalDate objects
+    val expiryLocalDate: LocalDate = LocalDate.parse(expiryDate.split("T")(0), dateFormatter)
+    val transactionLocalDate: LocalDate = LocalDate.parse(timestamp.split("T")(0), dateFormatter)
+  }
+
+  // Define a case class to represent discount rules
+  case class DiscountRule(qualifyingRule: Transaction => Boolean, calculationRule: Transaction => Double)
+
+  // Define a list of discount rules
+  val discountRules: List[DiscountRule] = List(
+    // Rule 1: Discount based on remaining days until expiry
+    DiscountRule(transaction => {
+      val expiry = transaction.expiryLocalDate
+      val transactionDate = transaction.transactionLocalDate
+      Period.between(transactionDate, expiry).getDays < 30
+    },
+      transaction => {
+        val expiry = transaction.expiryLocalDate
+        val transactionDate = transaction.transactionLocalDate
+        val daysRemaining = java.time.temporal.ChronoUnit.DAYS.between(transactionDate, expiry).toInt
+
+        if (daysRemaining >= 30) 0.0
+        else if (daysRemaining >= 1) {0.30 - (daysRemaining * 0.01)}
+        else 0.0
+      }),
+
+    // Rule 2: Discount for purchasing cheese & wine
+    DiscountRule(transaction => transaction.productName.toLowerCase.contains("cheese"), _ => 0.10),
+    DiscountRule(transaction => transaction.productName.toLowerCase.contains("wine"), _ => 0.05),
+
+
+    // Rule 3: Special discount on a specific date
+    DiscountRule(transaction => {
+      val date = transaction.transactionLocalDate
+      date.getMonthValue == 3 && date.getDayOfMonth == 23
+    },
+      _ => 0.50),
+
+    // Rule 4: Quantity-based discounts
+    DiscountRule(transaction => transaction.quantity >= 6 && transaction.quantity <= 9, _ => 0.05),
+    DiscountRule(transaction => transaction.quantity >= 10 && transaction.quantity <= 14, _ => 0.07),
+    DiscountRule(transaction => transaction.quantity >= 15, _ => 0.10),
+
+    // Rule 5: Discount based on purchase channel
+    DiscountRule(transaction => transaction.channel.toLowerCase == "app", transaction => {
+      val roundedQuantity =( Math.ceil(transaction.quantity.toDouble / 5) * 5 )/100
+      roundedQuantity
+    }),
+
+    // Rule 6: Discount for using Visa
+    DiscountRule(transaction => transaction.paymentMethod.toLowerCase == "visa", _ => 0.05)
+  )
+
+  // Function to calculate the total discount for a transaction
+  def calculateDiscount(transaction: Transaction): Double = {
+    // Filter discount rules that apply to the transaction
+    val qualifiedRules = discountRules.filter(_.qualifyingRule(transaction))
+
+    // Get discounts applicable to the transaction and sort them in descending order
+    val discounts = qualifiedRules.map(_.calculationRule(transaction)).filter(_>0).sorted.reverse
+
+    // Apply a maximum of two discounts
+    if (discounts.length >= 2) {
+      (discounts.take(2).sum / 2.0)
+    } else {
+      discounts.headOption.getOrElse(0.0)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    // Input file path
+    val inputFilePath = "D:/1/18.Scala/Retail_store_discount/src/main/resources/TRX1000.csv"
+    // Output file path
+    val outputFilePath = "D:/1/18.Scala/Retail_store_discount/src/main/resources/output.csv"
+    // Log file path
+    val logFilePath = "D:/1/18.Scala/Retail_store_discount/src/main/resources/rules_engine.log"
+
+    // Read transactions from input file
+    val transactions = scala.io.Source.fromFile(inputFilePath).getLines().drop(1).map(line => {
+      val cols = line.split(",")
+      Transaction(cols(0), cols(1), cols(2), cols(3).toInt, cols(4).toDouble, cols(5), cols(6))
+    }).toList
+
+    // Calculate discounts and generate output
+    val output = transactions.map(transaction => {
+      val discount = calculateDiscount(transaction)
+      val totalPriceAfterDiscount = transaction.quantity * transaction.unitPrice * (1 - discount)
+      s"${transaction.timestamp},${transaction.productName},${transaction.expiryDate},${transaction.quantity},${transaction.unitPrice},${transaction.channel},${transaction.paymentMethod},$discount,$totalPriceAfterDiscount"
+    })
+
+    // Generate log messages
+    val log = output.map(line => {
+      val timestamp = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+      s"$timestamp,INFO,$line"
+    })
+
+    // Write output to file
+    val writer = new PrintWriter(new File(outputFilePath))
+    writer.write("timestamp,product_name,expiry_date,quantity,unit_price,channel,payment_method,discount,total_price_after_discount\n")
+    output.foreach(writer.println)
+    writer.close()
+
+    // Write log messages to file
+    val logWriter = new PrintWriter(new File(logFilePath))
+    log.foreach(logWriter.println)
+    logWriter.close()
+  }
+}
